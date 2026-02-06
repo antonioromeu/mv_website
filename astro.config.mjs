8634c9838fb55ccfdae3d973015a845ffd1ab207
antonioromeu <romeupinheiro@icloud.com>
2026-02-06 00:33:57 +0000

    add configurations on astro.config.mks

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   // Replace with your info
+  build: {
+    // 4. Change '_astro' to 'assets' to avoid GitHub's Jekyll filter
+    assets: 'assets'
+  },
   site: 'https://antonioromeu.github.io', 
   base: '/mv_website',
   outDir: 'docs',
