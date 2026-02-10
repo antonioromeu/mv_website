ea6442f2be7acecb057d10c2cb3287b382f27447
antonioromeu <romeupinheiro@icloud.com>
2026-02-10 23:01:50 +0000

    removes blur on video cards and fixes favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // Replace with your info
-  build: {
-    // 4. Change '_astro' to 'assets' to avoid GitHub's Jekyll filter
-    assets: 'assets'
-  },
-  site: 'http://marioveloso.pt', 
-//   base: '/mv_website',
-  outDir: 'docs',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    build: {
+        assets: 'assets' // Change '_astro' to 'assets' to avoid GitHub's Jekyll filter
+    },
+    site: 'http://marioveloso.pt',
+    outDir: 'docs',
+    vite: {
+        plugins: [tailwindcss()]
+    }
 });
